7a6ceb284b6c21ef985223ff7f780a3c1fef1774
Shlomooo <61390728+Shlomooo@users.noreply.github.com>
2020-09-09 23:39:04 +0300

    Little modification in time plot
    insert state space without spring constant

--- Pendulum/time_plot_2.py
+++ Pendulum/time_plot_2.py
@@ -1,6 +1,9 @@
 from numpy import sin, cos
 import numpy as np
 import scipy.integrate as integrate
+import matplotlib.pyplot as plt
+from scipy.optimize import curve_fit
+
 
 # Pendulum Set up
 class pendulum:
@@ -14,6 +17,9 @@ class pendulum:
 # m: mass of pendulum 1 in kg
 # c: Damping of the joint
 # Environmental Constant: acceleration due to gravity, in m/s^2
+
+def fitfunc(x, a, b, c):
+    return a * np.sin(b * x) + c
 
 pen1 = pendulum(1,1,0,9.8)
 
@@ -45,3 +51,29 @@ th = integrate.odeint(derivs, state, t)
 
 x = pen1.l*sin(th[:, 1])
 y = -pen1.l*cos(th[:, 1])
+
+
+popt, pcov = curve_fit(fitfunc, t, y)  # popt- fitting parameters, pcov- covariance
+plt.plot(t, y, 'b-', label='data')
+plt.plot(t, fitfunc(t, *popt), 'r-',
+         label='fit: a=%5.3f, b=%5.3f, c=%5.3f' % tuple(popt))
+plt.xlabel('t')
+plt.ylabel('y')
+plt.legend(loc='upper right')
+
+plt.show()
+
+fig, axs = plt.subplots(1, 2)
+plot1, = axs[0].plot(t, x)
+axs[0].set_title('x over time')
+axs[0].set_xlabel('time [s] ')
+axs[0].set_ylabel('x [m]')
+plot2, = axs[1].plot(t, y)
+axs[1].set_title('y over time')
+axs[1].set_xlabel('time [s] ')
+axs[1].set_ylabel('y [m]')
+
+
+
+plt.tight_layout()
+plt.show()
