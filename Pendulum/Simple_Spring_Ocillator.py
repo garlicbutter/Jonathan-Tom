51347701824fa2ee412f8cdad4e0dfaefcf6d154
garlicbutter <51944853+garlicbutter@users.noreply.github.com>
2020-09-15 09:28:41 +0300

    fft for spring SHM
    2pi*sqrt(m/k)

--- Pendulum/Simple_Spring_Ocillator.py
+++ Pendulum/Simple_Spring_Ocillator.py
@@ -1,14 +1,16 @@
 from numpy import sin, cos
 import numpy as np
 import scipy.integrate as integrate
+from scipy.fft import fft, fftfreq
+import matplotlib.pyplot as plt
+
 
 
 # Pendulum Set up
-class pendulum:
-   def __init__(self,l,m,c,k):
+class spring:
+   def __init__(self, l, m, k):
        self.l = l
        self.m = m
-       self.c = c
        self.k = k
 
 # l: initial length of pendulum 1 in m
@@ -16,46 +18,69 @@ class pendulum:
 # c: Damping of the joint
 # k: Spring constant of the pendulum rod
 
-pen1 = pendulum(2,1,0,50)
+spring1 = spring(2,1,50)
 
 # Environmental Constant
 g = 0  # acceleration due to gravity, in m/s^2
 
 def derivs(state, t):
 
-    dthdt = np.zeros_like(state)
+    dxdt = np.zeros_like(state)
 
-    dthdt[0] = - g/(state[3]+pen1.l) * np.sin(state[1]) - 2 * state[2] * state[0] / (state[3]+pen1.l) - pen1.c/pen1.m * state[0]
+    dxdt[0] = -spring1.k/spring1.m*(state[1]-spring1.l)
 
-    dthdt[1] = state[0]
+    dxdt[1] = state[0]
 
-    dthdt[2] = (pen1.l+state[3])*state[0]**2 - pen1.k / pen1.m * state[3] + g * np.cos(state[1])
 
-    dthdt[3] = state[2]
-
-    return dthdt
+    return dxdt
 
 #time array from 0..100 sampled at 0.05 second steps
 dt = 0.01
 t = np.arange(0, 20, dt)
 
 # initial conditions
-# th is initial angle,  w is initial angular velocitie
-# l0 is the initial length of the rod, v0 is the initial longitudial velocity of the pendulum
-w0 = 0
-th0 = 90
-v0 = 10
-l0 = 0
+x0 = 5
+v0 = 0
 
 # initial value for state vectors
-state = [np.radians(w0),np.radians(th0),v0, l0]
+state = [v0, x0]
 
 # integrate ODE to obtain the angle values
-th = integrate.odeint(derivs, state, t)
+sol = integrate.odeint(derivs, state, t)
+x_sol, v_sol = sol[:,1], sol[:,0]
 
-x = (pen1.l+th[:,3])*sin(th[:, 1])
-y = -(pen1.l+th[:,3])*cos(th[:, 1])
+# time plot, x and y
+fig, axs = plt.subplots(2, 1)
+plot1, = axs[0].plot(t, x_sol)
+axs[0].set_title('x over time')
+axs[0].set_xlabel('time [s] ')
+axs[0].set_ylabel('x [m]')
+plot2, = axs[1].plot(t, v_sol)
+axs[1].set_title('v over time')
+axs[1].set_xlabel('time [s] ')
+axs[1].set_ylabel('v [m/s]')
+plt.tight_layout()
+plt.show()
 
+# Do FFT analysis of array
+FFT = fft(x_sol)
 
+# Getting the related frequencies
+freqs = fftfreq(len(x_sol), dt)     ## added dt, so x-axis is in meaningful units
 
+# Create subplot windows and show plot
+fig, axs = plt.subplots(2, 1)
+plot1, = axs[0].plot(t, x_sol)
+axs[0].set_title(r'x over time where $x_0 = $ %5.3f' %(x0))
+axs[0].set_xlabel('Time')
+axs[0].set_ylabel('Amplitude')
+plot2, = axs[1].plot(freqs, np.log10(abs(FFT)), '.', label = 'fft')
+plot3 = axs[1].axvline(1/(2*np.pi*np.sqrt(spring1.m/spring1.k)), 0.1, 0.9, color = 'k', label = r'$\frac{1}{2\pi}\sqrt{k/m}$')
+plot4 = axs[1].axvline(1/(-2*np.pi*np.sqrt(spring1.m/spring1.k)), 0.1, 0.9, color = 'k')
+axs[1].legend()
+axs[1].set_title('FFT analysis')
+axs[1].set_xlabel('Frequencies [Hz]')
+axs[1].set_ylabel('Amplitude (log10)')
+plt.tight_layout()
+plt.show()
 
