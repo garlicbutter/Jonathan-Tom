72d4982be72934dc0ed39a52bdd1acc47e3eacce
garlicbutter <51944853+garlicbutter@users.noreply.github.com>
2020-10-01 20:56:47 +0300

    PBIC - prototype

--- PBIC Training/PBIController.m
+++ PBIC Training/PBIController.m
@@ -1,6 +1,29 @@
 function [T] = PBIController(z,p)
 %Controller that uses DB-IC to calculate the torque needed
 
+th1 = z(1);
+th2 = z(3);
+thdot1 = z(2);
+thdot2 = z(4);
+
+%Current disturbance force on end effector
+FxCurrent = p.Fx;
+FyCurrent = p.Fy;
+
+%Current Target
+xCurrentTar = p.xtarget;
+yCurrentTar = p.ytarget;
+
+xdotCurrentTar = 0;
+ydotCurrentTar = 0;
+
+%Torque to track our desired point
+
+
+
+%Add gravity compensation
+T(1) = T(1) + GravityCompT1(0,0,p.d1,p.d2,p.g,p.l1,p.l2,p.m1,p.m2,th1,th2,thdot1,thdot2);
+T(2) = T(2) + GravityCompT2(0,0,p.d2,p.g,p.l1,p.l2,p.m2,th1,th2,thdot1);
 
 
 end
