6c64cf3a96e6ee91dad4fd46019e10445a66478c
garlicbutter <jonathan980213@yahoo.com.tw>
2020-12-30 19:00:52 +0200

    changed quickstart_jonathan"

--- myROBOSUITE/quickstart_jonathan.py
+++ myROBOSUITE/quickstart_jonathan.py
@@ -1,38 +1,53 @@
 import numpy as np
-import robosuite as suite
-from robosuite import load_controller_config
-from robosuite.models import MujocoWorldBase
-from robosuite.models.robots import UR5e
-from robosuite.models.grippers import gripper_factory
-from robosuite.models.arenas import TableArena
-from robosuite.models.objects import BoxObject
-from robosuite.utils.mjcf_utils import new_joint
 from mujoco_py import MjSim, MjViewer
+from robosuite.models.grippers import gripper_factory
+from robosuite.models import MujocoWorldBase
+from robosuite.models.arenas import TableArena
+from robosuite.models.robots import UR5e
 
-
+# Step 1: Creating the world. All mujoco object definitions are housed in an xml. We create a MujocoWorldBase class to do it.
 world = MujocoWorldBase()
-mujoco_robot = UR5e()
-gripper = gripper_factory('JacoThreeFingerGripper')
-mujoco_robot.add_gripper(gripper)
-mujoco_robot.set_base_xpos([0, 0, 0])
-world.merge(mujoco_robot)
 
-mujoco_arena = TableArena()
-mujoco_arena.set_origin([0.8, 0, 0])
+# Step 2: Creating the table. We can initialize the TableArena instance that creates a table and the floorplane
+# load model for table top workspace
+table_full_size = (0.8, 0.8, 0.05)
+table_friction = (1., 5e-3, 1e-4)
+table_offset = np.array((0, 0, 0.8))
+
+mujoco_arena = TableArena(
+    table_full_size=table_full_size,
+    table_friction=table_friction,
+    table_offset=table_offset,
+)
+
+# Arena always gets set to zero origin
+mujoco_arena.set_origin([0, 0, 0])
 world.merge(mujoco_arena)
 
-box1 = BoxObject(
-    name="Box",
-    size=[0.05, 0.08, 0.03],
-    rgba=[0, 0.5, 0.5, 1],
-    density=1,
-    joints="default",
-    duplicate_collision_geoms=True)
 
-box1.set('pos', '1.0 0 1.0')
-world.worldbody.append(box1)
+# Step 3: Creating the robot. The class housing the xml of a robot can be created as follows.
+mujoco_robot = UR5e()
 
+# We can add a gripper to the robot by creating a gripper instance and calling the add_gripper method on a robot.
+gripper = gripper_factory("Robotiq85Gripper")
+mujoco_robot.add_gripper(gripper)
+
+# To add the robot to the world, we place the robot on to a desired position and merge it into the world
+xpos = table_offset.tolist()
+mujoco_robot.set_base_xpos(xpos)
+world.merge(mujoco_robot)
+
+
+# Step 4: Adding the object.
+# For details of MujocoObject, refer to the documentations about MujocoObject, we can create a ball and add it to the world.
+# It is a bit more complicated than before because we are adding a free joint to the object (so it can move) and we want to place the object properly.
+
+
+# Step 5: Running Simulation. Once we have created the object, we can obtain a mujoco_py model by running
 model = world.get_model(mode="mujoco_py")
+
+# This is an MjModel instance that can then be used for simulation. For example,
+
 
 sim = MjSim(model)
 viewer = MjViewer(sim)
@@ -42,28 +57,3 @@ for i in range(10000):
     sim.data.ctrl[:] = 0
     sim.step()
     viewer.render()
-
-
-# Load the desired controller's default config as a dict
-# controller_config = load_controller_config(default_controller="JOINT_VELOCITY")
-
-
-# create environment instance
-# env = suite.make(
-#     env_name="Lift",  # try with other tasks like "Stack" and "Door"
-#     robots="UR5e",  # try with other robots like "Sawyer" and "Jaco"
-#     controller_configs=controller_config,
-#     has_renderer=True,
-#     has_offscreen_renderer=False,
-#     use_camera_obs=False,
-# )
-
-
-# reset the environment
-# env.reset()
-
-# for i in range(1000):
-#     action = np.random.randn(env.robots[0].dof)  # sample random action
-#     # take action in the environment
-#     obs, reward, done, info = env.step(action)
-#     env.render()  # render on display
