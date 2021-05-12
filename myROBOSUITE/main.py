42479af7e276dafa06a0148babd71880165fe3b4
garlicbutter <ckjonathan123@gmail.com>
2021-05-12 20:39:43 +0300

    simple grab

--- myROBOSUITE/main.py
+++ myROBOSUITE/main.py
@@ -26,9 +26,39 @@ if __name__ == "__main__":
 	# done = True when the task is completed
 	done = False
 
+	# Initial action
+	action = np.zeros(dof)
+	grabbed = False
+
 	# simulation loop
 	while not done:
-		# action = np.zeros(dof) 					
-		action = np.array([0, 0, 0, 0, 0, 0, 0])
-		obs, reward, done, info = env.step(action)  # take action in the environment
-		env.render()  								# render
+		obs, reward, done, _ = env.step(action)	# take action in the environment
+		env.render()  							# render
+
+		# observed value
+		eef_pos 		= obs['robot0_eef_pos'] 					# array 1x3
+		eef_quat 		= obs['robot0_eef_quat'] 					# array 1x4
+		eef_quat[0],eef_quat[1]	 = eef_quat[1],eef_quat[0]	
+		eef_quat[2]		= -eef_quat[2]
+
+		peg_pos 		= obs['peg_pos'] + np.array([0,0,0.025])		# array 1x3
+		peg_quat 		= obs['peg_quat']							# array 1X4
+		eef_to_peg_pos 	= peg_pos - eef_pos							# array 1x3
+		eef_to_peg_quat = peg_quat - eef_quat						# array 1x3
+		# dt				= env.control_timestep
+		
+		if grabbed:
+			action = np.array([0,0,0.2,0,0,0,1])
+
+		if np.linalg.norm(eef_to_peg_pos) > 0.01 and not grabbed:
+			action = np.concatenate( (eef_to_peg_pos, 0.2*np.array([0,0,eef_to_peg_quat[2],0])) )
+		elif np.linalg.norm(eef_to_peg_pos) < 0.01 and not grabbed:
+			action = np.concatenate( (eef_to_peg_pos, np.array([0,0,0,1]) ) ) 
+			grabbed = True
+
+
+
+
+
+		
+
